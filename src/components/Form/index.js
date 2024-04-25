be46d4a0311dff8504c7d36db7e7ad020dc85fab
zSm0k3 <thexof02@gmail.com>
2024-04-25 10:40:11 -0300

    25_04

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { TextInput } from "react-native-web";
+import React, {useState} from "react";
+import { View, Text, TextInput, Button } from "react-native";
+
 
 export default function Form(){
+
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("preencha seu peso e sua altura");
+    const [imc, setIMC] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular IMC")
+
     return(
         <View>
             <View>
@@ -20,6 +27,12 @@ export default function Form(){
                 placeholder="Ex. 65.60"
                 keyboardType="Numeric"
                 />
+
+                <Button
+                    title={textButton}
+                onPress={() => Alert.alert('Simple Button pressed')}
+                />
+
             </View>
         </View>
     );
