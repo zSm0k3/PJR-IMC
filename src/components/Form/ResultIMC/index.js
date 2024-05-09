82ea3326a668b9a7808f7d96a37ef88cfdb8064a
zSm0k3 <thexof02@gmail.com>
2024-05-09 10:33:45 -0300

    Resultado

---
+++ src/components/Form/ResultIMC/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ResultIMC(props){
+    return(
+        <View>
+            <Text>
+                {props.messageResultIMC}
+            </Text>
+            
+            <Text>
+                {props.ResultIMC}
+            </Text>
+        </View>
+    );
+};
