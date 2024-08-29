a34fcfae4a3ee7c12c20f80e05bcd088bb44d070
zSm0k3 <160192976+zSm0k3@users.noreply.github.com>
2024-08-29 10:45:14 -0300

    queridissimo

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 
 import { StyleSheet, Text, View } from 'react-native';
-import Title from './src/components/Title';
-import Main from './src/components/Main';
+import Title from './src/components/Title/index';
+import Form from './src/components/form';
 
 export default function App() {
   return (
     <View style={styles.container}>
+   
       <Title/>
-      <Main/>
-    </View>
+      <Form/>
+    
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e0e5e5',
+    paddingTop: 80,
+ 
   },
 });
